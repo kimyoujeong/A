760bbab1890aa4d71fc52cc3ad9470339366c8d1
kimyoujeong <55773434+kimyoujeong@users.noreply.github.com>
2021-01-02 17:20:57 +0900

    학점

---
+++ src/javaro/a.java
@@ -0,0 +1,15 @@
+package javaro;
+
+public class a {
+public static void main(String[] args) {
+	int score=95;
+	if(score>=90)
+	{
+		System.out.println("A 학점");
+	}
+	else
+	{
+		System.out.println("A 학점이 아님");
+	}
+}
+}
